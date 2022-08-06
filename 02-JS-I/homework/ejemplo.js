6088cd3ef46bab031cc62548a259c882aad6f5b0
miguel <lugo19942011@hotmail.com>
2022-08-06 18:40:12 +0200

    homework JSI

---
+++ 02-JS-I/homework/ejemplo.js
@@ -0,0 +1,18 @@
+
+var suma = 'jose '
+
+//  console.log(suma)
+
+ var suma2 = 'pablo'
+
+ var total = suma + suma2
+
+ console.log(21%5)
+ 
+
+ var otraFunc = (n, m) => n*m
+
+ console.log(otraFunc(6, 7))
+ console.log(otraFunc(8, 9))
+ console.log(otraFunc(78, 3))
+ console.log(otraFunc(86, 13))
